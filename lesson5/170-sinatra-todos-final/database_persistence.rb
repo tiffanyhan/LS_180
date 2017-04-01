bab025d8ae8cda2644aa68cf95ee0fd6e60ca5aa
Tiffany Han <hantiffany@gmail.com>
2017-03-31 21:38:53 -0700

    complete lesson6

--- lesson5/170-sinatra-todos-final/database_persistence.rb
+++ lesson5/170-sinatra-todos-final/database_persistence.rb
@@ -1,4 +1,5 @@
 require 'pg'
+require 'pry'
 
 class DatabasePersistence
   def initialize(logger)
@@ -7,24 +8,37 @@ class DatabasePersistence
   end
 
   def find_list(id)
-    sql = "SELECT * FROM lists WHERE id = $1"
+    sql = <<~SQL
+      SELECT lists.*,
+        count(todos.id) AS todos_count,
+        count( NULLIF(todos.completed, true) ) AS todos_remaining_count
+        FROM lists
+        LEFT JOIN todos
+        ON lists.id = todos.list_id
+        WHERE lists.id = $1
+        GROUP BY lists.id
+        ORDER BY lists.name;
+    SQL
     result = query(sql, id)
 
-    row = result.first
-    list_id = row['id'].to_i
-    todos = get_todos(list_id)
-
-    { id: list_id, name: row['name'], todos: todos }
+    row_to_list_hash(result.first)
   end
 
   def all_lists
-    sql = "SELECT * FROM lists"
+    sql = <<~SQL
+      SELECT lists.*,
+        count(todos.id) AS todos_count,
+        count( NULLIF(todos.completed, true) ) AS todos_remaining_count
+        FROM lists
+        LEFT JOIN todos
+        ON lists.id = todos.list_id
+        GROUP BY lists.id
+        ORDER BY lists.name;
+    SQL
     result = query(sql)
 
     result.map do |row|
-      list_id = row['id'].to_i
-      todos = get_todos(list_id)
-      { id: list_id, name: row['name'], todos: todos }
+      row_to_list_hash(row)
     end
   end
 
@@ -41,6 +55,18 @@ class DatabasePersistence
   def delete_list(id)
     sql = "DELETE FROM lists WHERE id = $1"
     query(sql, id)
+  end
+
+  def get_todos(list_id)
+    sql = "SELECT * FROM todos WHERE list_id = $1"
+    result = query(sql, list_id)
+
+    result.map do |row|
+      { id: row['id'].to_i,
+        name: row['name'],
+        completed: row['completed'] == 't'
+      }
+    end
   end
 
   def create_new_todo(list_id, text)
@@ -65,20 +91,16 @@ class DatabasePersistence
 
   private
 
+  def row_to_list_hash(row)
+    { id: row['id'],
+      name: row['name'],
+      todos_count: row['todos_count'].to_i,
+      todos_remaining_count: row['todos_remaining_count'].to_i
+    }
+  end
+
   def query(statement, *params)
     @logger.info "#{statement}: #{params}"
     @db.exec_params(statement, params)
-  end
-
-  def get_todos(list_id)
-    sql = "SELECT * FROM todos WHERE list_id = $1"
-    result = query(sql, list_id)
-
-    result.map do |row|
-      { id: row['id'].to_i,
-        name: row['name'],
-        completed: row['completed'] == 't'
-      }
-    end
   end
 end
